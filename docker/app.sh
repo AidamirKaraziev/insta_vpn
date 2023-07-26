4cb583b8bc0592047bc67cd5fc3db9d518f4f3a2
Aydamir Karaziev <ajdamir.karaziev@gmail.com>
2023-07-27 01:26:36 +0300

    add alembic env and update app.sh

--- docker/app.sh
+++ docker/app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#alembic revision --autogenerate -m "Database creation"
+alembic revision --autogenerate -m "Database creation"
 alembic upgrade head
 
 cd src
 
-gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 --log-level debug --access-logfile -
+gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:55713 --log-level debug --access-logfile -
