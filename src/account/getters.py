e1ddfb0b5d2b9e4d2f27d3ef66d133a35fdee837
Aydamir Karaziev <ajdamir.karaziev@gmail.com>
2023-10-02 18:52:44 +0300

    fix default datetime.utcnow

--- src/account/getters.py
+++ src/account/getters.py
@@ -3,10 +3,11 @@ from account.schemas import AccountGet
 
 
 def getting_account(obj: AccountGet) -> Optional[AccountGet]:
-    formatted_timestamp = obj.created_at.strftime('%Y-%m-%d %H:%M:%S')
+    if obj.created_at is not None:
+        obj.created_at = obj.created_at.strftime('%Y-%m-%d %H:%M:%S')
     return AccountGet(
         id=obj.id,
         name=obj.name,
         number=obj.number,
-        created_at=formatted_timestamp
+        created_at=obj.created_at
     )
--- src/account/models.py
+++ src/account/models.py
@@ -14,4 +14,4 @@ class Account(Base):
     id = Column(BigInteger, primary_key=True, autoincrement=False)
     name = Column(String)
     number = Column(String)
-    created_at = Column(TIMESTAMP(timezone=False), default=datetime.now())
+    created_at = Column(TIMESTAMP, default=datetime.utcnow)
