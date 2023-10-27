c4c8c5872b5d22b363590df0d604ae821bc7e639
Aydamir Karaziev <ajdamir.karaziev@gmail.com>
2023-10-27 22:58:24 +0300

    remove unused code in utils - add admin empty router

---
+++ src/admin/router.py
@@ -0,0 +1,80 @@
+import logging
+from datetime import datetime
+
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import UUID4
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.raise_template import get_raise_new, raise_schemas
+from core.response import SingleEntityResponse, ListOfEntityResponse, OkResponse
+from database import get_async_session
+from server.crud import crud_server
+from server.getters import getting_server
+from server.schemas import ServerCreate
+from static_key.crud import crud_static_key
+from auth.base_config import fastapi_users
+from auth.models import User
+
+current_active_superuser = fastapi_users.current_user(active=True, superuser=True)
+
+
+router = APIRouter(
+    prefix="/admin",
+    tags=["Admin"]
+)
+
+
+# здесь будут все администраторские апи
+@router.post(path="/",
+             response_model=SingleEntityResponse,
+             name='add_server_create_keys',
+             description='Добавить сервер и создать ключи для него'
+             )
+async def add_server_create_keys(
+        new_data: ServerCreate,
+        user: User = Depends(current_active_superuser),
+        session: AsyncSession = Depends(get_async_session),
+):
+    try:
+        server, code, indexes = await crud_server.add_server(db=session, new_data=new_data)
+        await get_raise_new(code)
+        keys, code, indexes = await crud_static_key.creating_keys_for_a_server(db=session, server_id=server.id)
+        await get_raise_new(code)
+        return SingleEntityResponse(data=getting_server(obj=server))
+    except Exception as ex:
+        await get_raise_new(raise_schemas(ex))
+
+
+if __name__ == "__main__":
+    logging.info('Running...')
+
+"""ВАЖНО"""
+# TODO У нас изменился profile_id. квитанции оплаты привязаны к этим ID. Что будет если тормоза решатся оплатить?)))
+#  можно ли очистить диалог у тормозов?
+#  возможно дописать код, чтобы проходила проверка на "тормоза"?
+
+
+"""Функционал который требует обсуждения"""
+# TODO сделать одну почту для всех созданных серверов
+# TODO Можем ли мы получить инфу когда человек отключается от впн в приложении???(Нажатие кнопки)
+
+
+"""Celery"""
+# TODO отключение неоплаченных профилей
+# TODO CELERY сделать рассылку сообщений пользователям
+
+
+"""Реферальная программа"""
+# TODO Написать блок схему
+
+
+"""Утвержденные задачи -> перенести в Notion"""
+# TODO добавить комментарии к каждой функции и методу
+# TODO регистрация - бесплатный период, код с настройками - оплата - реферальная программа
+# TODO добавить поле в аккаунт trial, referral
+# TODO надо ли ограничить количество создаваемых профилей? думаю да чтобы ебланы и ебланнесы не создавали по
+#  2000 неоплаченных профилей      ОГРАНИЧИТЬ до 5
+# TODO написать диагностику впн серверов. сделать это отдельно
+# TODO автоматические выплаты
+# TODO сделать вывод у кого
+
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -22,54 +22,6 @@ from server.schemas import ServerUpdate
 """
 
 
-async def update_used_bytes_in_profiles(db: AsyncSession, skip: int = 0):
-    servers, code, indexes = await crud_server.get_all_servers(db=db, skip=skip, limit=LIMIT_SERVERS)
-    for server in servers:
-        client = OutlineVPN(api_url=server.api_url, cert_sha256=server.cert_sha256)
-        try:
-            # проверка серверов, если не достучались до сервера -> показать его
-            keys = client.get_keys()
-
-            for key in keys:
-                profile, code, indexes = await crud_profile.get_profile_by_key_id_server_id(
-                    db=db, server_id=int(server.id), key_id=int(key.key_id))
-                if profile is not None:
-                    update_data = ProfileUpdate(used_bytes=key.used_bytes)
-                    obj, code, indexes = await crud_profile.update_profile(
-                        db=db, update_data=update_data, id=profile.id)
-        except Exception as ex:
-            print(ex)  # возможно записывать ошибки в логи
-    return None, 0, None
-
-
-async def get_keys_without_a_profile_and_bad_server(db: AsyncSession, skip: int = 0):
-    not_in_db = []
-    unavailable_server = {}
-    bad_servers = []
-    servers, code, indexes = await crud_server.get_all_servers(db=db, skip=skip, limit=LIMIT_SERVERS)
-    for server in servers:
-        try:
-            # проверка серверов, если не достучались до сервера -> показать его
-            client = OutlineVPN(api_url=server.api_url, cert_sha256=server.cert_sha256)
-            keys = client.get_keys()
-            for key in keys:
-                profile, code, indexes = await crud_profile.get_profile_by_key_id_server_id(db=db,
-                                                                                            key_id=int(key.key_id),
-                                                                                            server_id=int(server.id))
-                if profile is None:
-                    not_in_db.append(indexes)
-                if profile is not None:
-                    update_data = ProfileUpdate(used_bytes=key.used_bytes)
-                    obj, code, indexes = await crud_profile.update_profile(
-                        db=db, update_data=update_data, id=profile.id)
-        except Exception as ex:
-            # добавить плохой сервер сюда
-            unavailable_server[f"{server.id}, {server.name}"] = f"{ex}"
-    bad_servers.append(unavailable_server)
-    not_in_db.append(bad_servers)
-    return not_in_db, 0, None
-
-
 async def update_fact_clients(*, db: AsyncSession):
     # get all server
     bad_servers = {}
