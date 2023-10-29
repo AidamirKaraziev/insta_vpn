f343d0619fb9c0b168dfd761a0dd2ac8dd4ee7f2
azkaraz <karaziev988@gmail.com>
2023-10-29 09:31:03 +0700

    - change shedule

--- src/tasks/beat_tasks.py
+++ src/tasks/beat_tasks.py
@@ -4,9 +4,11 @@ from celery.schedules import crontab
 from config import REDIS_HOST, REDIS_PORT
 
 celery_beat = Celery('app_beat_tasks', broker=f'redis://{REDIS_HOST}:{REDIS_PORT}')
+celery_beat.conf.timezone = 'Europe/Moscow'
+
 celery_beat.conf.beat_schedule = {
     'describe_users': {
         'task': 'tasks.tasks.describe_users',
-        'schedule': crontab(hour=2, minute=19),
+        'schedule': crontab(hour=0, minute=0),
     },
 }
