11818fe2d831ca88c72f2b04a3d5bd1ae6a59d09
Aydamir Karaziev <ajdamir.karaziev@gmail.com>
2023-11-01 11:56:23 +0300

    test vless +3

--- src/profiles/dymamic.py
+++ src/profiles/dymamic.py
@@ -1,21 +1,39 @@
 import re
 
 from fastapi import APIRouter, Depends
+from fastapi import FastAPI, UploadFile, File, Response
 from pydantic import UUID4
+from requests import request
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.responses import HTMLResponse
 
 from core.raise_template import get_raise_new
 from database import get_async_session
 from profiles.crud import crud_profile
 from profiles.schemas import ProfileUpdate
 from static_key.crud import crud_static_key
+import base64
 
 router = APIRouter()
 
 
+@router.get("/encode",
+            response_class=HTMLResponse)
+def encode_string(response: Response):
+    string = "vless://a5397ed5-7479-4792-adc9-0fdb92c5cd90@176.57.212.124:443?type=tcp&security=reality&fp=firefox&pbk=n65lCyTgyOPYaRegNqxyfZfhBaGoP7JgLkXXQBnNvSw&sni=yahoo.com&sid=f8d8328e&spx=%2F#1-%D0%90%D0%B9%D0%B4%D0%B0%D0%BC%D0%B8%D1%80"
+    encoded_string = base64.b64encode(string.encode()).decode()
+
+    # Установите заголовок Content-Type на text/html
+    # response.headers["Content-Type"] = "text/html"
+
+    # Сформируйте ответ в виде HTML строки
+    html_response = encoded_string
+    return html_response
+
+
 @router.get(path='/conf/{profile_id}',
             name='outline_connect',
-            description='Подключение Outline '
+            description='Подключение Outline ',
             )
 async def handle_payment(
         profile_id: UUID4,
@@ -48,6 +66,12 @@ async def handle_payment(
 """TEST dynamic key VLESS"""
 
 
+def encode_to_base64(str):
+    encoded_bytes = base64.b64encode(str.encode('utf-8'))
+    encoded_str = encoded_bytes.decode('utf-8')
+    return encoded_str
+
+
 @router.get(path='/test-vless/',
             name='outline_connect',
             description='Подключение Outline'
@@ -55,72 +79,9 @@ async def handle_payment(
 async def handle_payment(
         session: AsyncSession = Depends(get_async_session),
 ):
-    res = {
-  "inbounds" : [
-    {
-      "listen" : "127.0.0.1",
-      "port" : 1080,
-      "protocol" : "socks",
-      "settings" : {
-        "auth" : "noauth",
-        "udp" : true
-      },
-      "sniffing" : {
-        "destOverride" : [
-          "http",
-          "tls",
-          "quic",
-          "fakedns"
-        ],
-        "enabled" : false,
-        "routeOnly" : true
-      },
-      "tag" : "socks"
-    }
-  ],
-  "outbounds" : [
-    {
-      "protocol" : "vless",
-      "settings" : {
-        "vnext" : [
-          {
-            "address" : "176.57.212.124",
-            "port" : 443,
-            "users" : [
-              {
-                "encryption" : "none",
-                "flow" : "xtls-rprx-vision",
-                "id" : "9c0b568d-446a-40c3-9514-bf6e724fec8a"
-              }
-            ]
-          }
-        ]
-      },
-      "streamSettings" : {
-        "network" : "tcp",
-        "realitySettings" : {
-          "fingerprint" : "firefox",
-          "publicKey" : "n65lCyTgyOPYaRegNqxyfZfhBaGoP7JgLkXXQBnNvSw",
-          "serverName" : "yahoo.com",
-          "shortId" : "f8d8328e",
-          "spiderX" : "\/"
-        },
-        "security" : "reality",
-        "tcpSettings" : {
+    my_string = "vless://9c0b568d-446a-40c3-9514-bf6e724fec8a@176.57.212.124:443?type=tcp&security=reality&fp=firefox&pbk=n65lCyTgyOPYaRegNqxyfZfhBaGoP7JgLkXXQBnNvSw&sni=yahoo.com&flow=xtls-rprx-vision&sid=f8d8328e&spx=%2F#1-test2"
+    encoded_string = encode_to_base64(my_string)
+    print(encoded_string)
 
-        }
-      },
-      "tag" : "proxy"
-    },
-    {
-      "protocol" : "freedom",
-      "tag" : "direct"
-    },
-    {
-      "protocol" : "blackhole",
-      "tag" : "block"
-    }
-  ]
-}
+    return encoded_string
 
-    return res
