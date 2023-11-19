c08357b4bb2bfb8c54de7dcadfcfab17ba4ea8e8
azkaraz <karaziev988@gmail.com>
2023-11-19 15:39:49 +0700

    - change celery.sh

--- docker/celery.sh
+++ docker/celery.sh
@@ -7,5 +7,5 @@ if [[ "${1}" == "celery" ]]; then
 elif [[ "${1}" == "flower" ]]; then
   celery --app=tasks.tasks:celery flower
 elif [[ "${1}" == "beat" ]]; then
-  celery --app=tasks.beat_tasks:celery_beat beat -l INFO
+  celery --app=tasks.celery_beat:celery_beat beat -l INFO
 fi
