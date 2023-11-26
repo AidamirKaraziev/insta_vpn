ca7254c257b094e7b8177b3c303d9a79e88c46c7
ajdamir.karaziev <ajdamir.karaziev@gmail.com>
2023-11-27 02:56:06 +0300

    add auto creation referent_type

--- src/core/initial_data.py
+++ src/core/initial_data.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
 from database import get_async_session
 from partner.models import Partner
+from referent_type.models import ReferentType
 from status.models import Status
 from tariff.models import Tariff
 from vpn_type.models import VpnType
@@ -11,11 +12,11 @@ from vpn_type.models import VpnType
 # TODO внедрить проверку на исключения через try: except:
 
 async def check_vpn_type(session: AsyncSession = Depends(get_async_session)):
-    vpn_type_check_list = [VpnType(id=1, name='Outline'),
+    check_list = [VpnType(id=1, name='Outline'),
                            VpnType(id=2, name='VLESS')]
 
     creation_list = []
-    for obj in vpn_type_check_list:
+    for obj in check_list:
         query = select(VpnType).where(VpnType.name == obj.name, VpnType.id == obj.id)
         obj_type = await session.execute(query)
         if obj_type.scalar_one_or_none() is None:
@@ -32,7 +33,7 @@ async def create_vpn_type():
 
 
 async def check_tariff(session: AsyncSession = Depends(get_async_session)):
-    tariff_check_list = [
+    check_list = [
         Tariff(id=1, name='30 дней (100₽)', price=100, period_days=30, photo_url='https://i.imgur.com/e4pGpmu.jpg',
                is_active=True),
         Tariff(id=2, name='60 дней (200₽)', price=200, period_days=60, photo_url='https://i.imgur.com/nld6nHb.jpg',
@@ -41,7 +42,7 @@ async def check_tariff(session: AsyncSession = Depends(get_async_session)):
                is_active=True)]
 
     creation_list = []
-    for obj in tariff_check_list:
+    for obj in check_list:
         query = select(Tariff).where(
             Tariff.id == obj.id, Tariff.name == obj.name, Tariff.price == obj.price,
             Tariff.period_days == obj.period_days, Tariff.photo_url == obj.photo_url, Tariff.is_active == obj.is_active)
@@ -107,8 +108,30 @@ async def create_status():
         await db.close()
 
 
+async def check_referent_type(session: AsyncSession = Depends(get_async_session)):
+    check_list = [ReferentType(id=1, name='Native'),
+                  ReferentType(id=2, name='Blogger')]
+
+    creation_list = []
+    for obj in check_list:
+        query = select(ReferentType).where(ReferentType.name == obj.name, ReferentType.id == obj.id)
+        obj_type = await session.execute(query)
+        if obj_type.scalar_one_or_none() is None:
+            creation_list.append(obj)
+    return creation_list
+
+
+async def create_referent_type():
+    async for db in get_async_session():
+        creation_list = await check_referent_type(db)
+        [db.add(obj) for obj in creation_list]
+        await db.commit()
+        await db.close()
+
+
 async def create_initial_data():
     await create_vpn_type()
     await create_tariff()
     await create_partner()
     await create_status()
+    await create_referent_type()
