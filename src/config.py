fed7bf8298fe46d8ea4622fd25122f68cd292475
ajdamir.karaziev <ajdamir.karaziev@gmail.com>
2023-12-01 16:14:34 +0300

    modify config`

--- src/config.py
+++ src/config.py
@@ -2,6 +2,11 @@ import os
 
 from dotenv import load_dotenv
 
+from partner.schemas import PartnerCreate
+from referent_type.schemas import ReferentTypeCreate
+from status.schemas import StatusCreate
+from tariff.schemas import TariffCreate
+
 load_dotenv()
 
 DB_HOST = os.environ.get("DB_HOST")
@@ -53,3 +58,28 @@ BASE_REGISTER_REFERENT_LINK = "https://t.me/financier_tgrm_bot?start="  # bot/re
 """Уже не нужны"""
 FREE_TRAFFIC = 1  # Максимальное количество байт для статического ключа
 PAYMENT_WAITING_TIME = 86400 * 2  # 1 day = 86400 РУДИМЕНТ -> УДАЛИТЬ
+
+
+"""Payment"""
+STATUS_CREATE = StatusCreate(id=1, name='Создан')
+STATUS_ERROR = StatusCreate(id=2, name='Ошибка')
+STATUS_DONE = StatusCreate(id=3, name='Готов')
+
+"""Partner"""
+GENERAL_PARTNER = PartnerCreate(id=1, name='Общие')
+AIDAMIR_PARTNER = PartnerCreate(id=2, name='Айдамир')
+AZAMAT_PARTNER = PartnerCreate(id=3, name='Азамат')
+NASTYA_PARTNER = PartnerCreate(id=4, name='Настя')
+KARA_PARTNER = PartnerCreate(id=5, name='Кара')
+
+"""ReferentType"""
+NATIVE_REFERENT_TYPE = ReferentTypeCreate(id=1, name='Native'),
+BLOGGER_REFERENT_TYPE = ReferentTypeCreate(id=2, name='Blogger')
+
+"""Tariff"""
+ONE_MONTH_TARIFF = TariffCreate(id=1, name='30 дней (100₽)', price=100, period_days=30,
+                                photo_url='https://i.imgur.com/e4pGpmu.jpg', is_active=True),
+TWO_MONTH_TARIFF = TariffCreate(id=2, name='60 дней (200₽)', price=200, period_days=60,
+                                photo_url='https://i.imgur.com/nld6nHb.jpg', is_active=True),
+THREE_MONTH_TARIFF = TariffCreate(id=3, name='90 дней (300₽)', price=300, period_days=90,
+                                  photo_url='https://i.imgur.com/jEcEWNj.jpg', is_active=True)
