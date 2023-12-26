89b04adf78ee356c65c351b1f42556c96d70f644
Karaji_jin2 <ajdamir.kareaziev@gmail.com>
2023-12-26 21:57:55 +0300

    remove BASE_PARTNER

--- src/config.py
+++ src/config.py
@@ -43,7 +43,6 @@ TRIAL_DAYS = 3  # Количество пробных дней
 
 """REFERENT"""
 BASE_REFERENT_GIFT_DAYS = 5  # базовое количество подарочных дней
-BASE_PARTNER = 1  # базовый партнер - лицо привело референта 1="Общие", нужно для KPI
 THE_AMOUNT_OF_PAYMENT_FOR_A_REFERRAL = 50  # Реферальная выплата за нового клиента
 
 OUTLINE_USERS_GATEWAY = os.environ.get("OUTLINE_USERS_GATEWAY")
